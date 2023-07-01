145fe5db6d4450b0c5b46ffadd53c0a286ca22a9
Dmitry <MichaelKnightRider3000@gmail.com>
2023-07-01 23:32:04 +0300

    New Version My App HomeWork

--- HomeWorkCodable/TableViewController.swift
+++ HomeWorkCodable/TableViewController.swift
@@ -20,28 +20,28 @@ class CarsViewController: UITableViewController {
             tableView.reloadData()
         }
     }
-
+    
     private weak var activity: UIActivityIndicatorView?
     
-    
     @IBAction func addNewCar(_ sender: Any) {
-        guard let url = URL(string: "https://my-json-server.typicode.com/iDmitry89/HomeWorkCodable/db") else {
+        guard let url = URL(string: "https://my-json-server.typicode.com/iDmitry89/HomeWorkCodable/data") else {
             return
         }
-        var requestPost = URLRequest(url: url)
-        requestPost.httpMethod = "POST"
-        requestPost.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        let newCar = Car(id: 55, name: "HUMMER")
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let newMarka = Car(id: 55, name: "HUMMER")
+        
         do {
-            let jsonBody = try JSONEncoder().encode(newCar)
-            requestPost.httpBody = jsonBody
+            let jsonBody = try JSONEncoder().encode(newMarka)
+            request.httpBody = jsonBody
         } catch {
             
         }
         let session = URLSession.shared
-        let task = session.dataTask(with: requestPost) {
-            (data, _, _) in
+        let task = session.dataTask(with: request) {
+            (data,_,_) in
             guard let data = data else { return }
             do {
                 let sendPost = try JSONDecoder().decode(Car.self, from: data)
@@ -51,43 +51,44 @@ class CarsViewController: UITableViewController {
             }
         }
         task.resume()
+        tableView.reloadData()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        let activity = UIActivityIndicatorView(style: .medium)
-//        activity.sizeToFit()
-//        activity.hidesWhenStopped = true
-//        self.activity = activity
-//
-//        let buttonItem = UIBarButtonItem(customView: activity)
-//        navigationItem.rightBarButtonItem = buttonItem
-//
-//        activity.startAnimating()
+        
+        //        let activity = UIActivityIndicatorView(style: .medium)
+        //        activity.sizeToFit()
+        //        activity.hidesWhenStopped = true
+        //        self.activity = activity
+        //
+        //        let buttonItem = UIBarButtonItem(customView: activity)
+        //        navigationItem.rightBarButtonItem = buttonItem
+        //
+        //        activity.startAnimating()
         carsService.fetchCars { result in
             //self.activity?.stopAnimating()
             switch result {
-                case .success(let cars):
-                    self.cars = cars
-                case .failure(let error):
-                    print(error.localizedDescription)
+            case .success(let cars):
+                self.cars = cars
+            case .failure(let error):
+                print(error.localizedDescription)
             }
         }
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cars.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? CustomTableViewCell else {
             return .init()
         }
-
+        
         let auto = cars[indexPath.row]
         cell.markaLabel.text = "\(auto.name)"
-
+        
         return cell
     }
     
@@ -95,13 +96,13 @@ class CarsViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if segue.identifier == "segueToDetail" {
-//            if let indexPath = tableView.indexPathForSelectedRow {
-//                let destinationController = segue.destination as! ModelsViewController
-//                    destinationController.auto = cars
-//            }
-//        }
-//    }
-
+    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //        if segue.identifier == "segueToDetail" {
+    //            if let indexPath = tableView.indexPathForSelectedRow {
+    //                let destinationController = segue.destination as! ModelsViewController
+    //                    destinationController.auto = cars
+    //            }
+    //        }
+    //    }
+    
 }
