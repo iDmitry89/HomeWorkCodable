104f58fdbb8e3e2263283d0b5461b9329c5895f6
Dmitry <MichaelKnightRider3000@gmail.com>
2023-07-08 20:06:28 +0300

    HomeWork

--- Garage/AddNewCar.swift
+++ Garage/AddNewCar.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 func addNewCar() {
-    guard let url = URL(string: "https://my-json-server.typicode.com/iDmitry89/HomeWorkCodable/db") else {
+    guard let url = URL(string: "https://my-json-server.typicode.com/posts") else {
         return
     }
     
@@ -18,39 +18,52 @@ func addNewCar() {
     
     let newMarka = Marka(id: 55, name: "KIA", model: Model(id: 55, model: "Mohave"))
     
-    guard let jsonBody = try? JSONEncoder().encode(newMarka) else {
+    guard let jsonBody = try? JSONSerialization.data(withJSONObject: newMarka) else {   ///Encoder().encode(newMarka) else {
         return
     }
         request.httpBody = jsonBody
 
-    let _ = URLSession.shared.uploadTask(with: request, from: jsonBody) { data, response, error in
-        if let error = error {
-            print ("error: \(error)")
-            return
+    let session = URLSession.shared ///.uploadTask(with: request, from: jsonBody) { data, response, error in
+    session.dataTask(with: request) { data, response, error in
+        if let response = response {
+            print(response)
         }
-        guard let response = response as? HTTPURLResponse,
-            (200...299).contains(response.statusCode) else {
-            print ("Server error")
-            return
-        }
-        if let mimeType = response.mimeType,
-            mimeType == "application/json",
-            let data = data,
-           let dataString = String(data: data, encoding: .utf8) {
-            print ("got data: \(dataString)")
-        }
-    }
-
-    let sessionGet = URLSession.shared
-    let task = sessionGet.dataTask(with: request) {
-        (data,_,_) in
         guard let data = data else { return }
         do {
-            let sendPost = try JSONDecoder().decode(Marka.self, from: data)
-            print(sendPost)
+            let json = try JSONSerialization.jsonObject(with: data)
+            print(data)
         } catch {
-
+            print(error)
         }
-    }
-    task.resume()
+    }.resume()
+        
+//        if let error = error {
+//            print ("error: \(error)")
+//            return
+//        }
+//        guard let response = response as? HTTPURLResponse,
+//            (200...299).contains(response.statusCode) else {
+//            print ("Server error")
+//            return
+//        }
+//        if let mimeType = response.mimeType,
+//            mimeType == "application/json",
+//            let data = data,
+//           let dataString = String(data: data, encoding: .utf8) {
+//            print ("got data: \(dataString)")
+//        }
+//    }
+//
+//    let sessionGet = URLSession.shared
+//    let task = sessionGet.dataTask(with: request) {
+//        (data,_,_) in
+//        guard let data = data else { return }
+//        do {
+//            let sendPost = try JSONDecoder().decode(Marka.self, from: data)
+//            print(sendPost)
+//        } catch {
+//
+//        }
+//    }
+//    task.resume()
 }
